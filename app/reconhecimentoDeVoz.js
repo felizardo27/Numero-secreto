95dbca5a16ee5b28df30afae36f7a65c4c00eb86
Joao Pedro Felizardo <jp.felizardo27@gmail.com>
2022-07-09 16:11:27 -0300

    definindo a voz com Web Speech

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoChute = document.querySelector('#chute');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', (e) => {
+    const chute = e.results[0][0].transcript;
+    exibeChute(chute);
+});
+
+function exibeChute(chute) {
+
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `;
+}
